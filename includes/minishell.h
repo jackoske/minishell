3559fd73f652b08c949b04d2762bf152606c74fc
VernihoraIvan <vernihorai@gmail.com>
2024-05-23 17:15:59 +0200

    adding basic spreading into tokens functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:11:41 by iverniho          #+#    #+#             */
-/*   Updated: 2024/05/22 15:29:29 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:25:11 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@
 # include "../lib/gnl/get_next_line.h"
 
 # define PROMPT "minishell> "
+
+typedef struct s_token
+{
+	char			*content;
+	struct s_token	*next;
+}	t_token;
 
 void	prompt_loop(void);
 int		quotes_closed(char *line);
--- src/parser/prompt.c
+++ src/parser/prompt.c
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 21:09:11 by Jskehan           #+#    #+#             */
-/*   Updated: 2024/05/22 17:55:55 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:14:57 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,89 @@ static char	*read_command(void)
 	return (input);
 }
 
+t_token	*ft_lstnew1(void *content)
+{
+	t_token	*item;
+
+	item = malloc(sizeof(t_token));
+	if (!item)
+		return (NULL);
+	item->content = content;
+	item->next = NULL;
+	return (item);
+}
+
+void	ft_lstadd_back1(t_token **alst, t_token *new)
+{
+	t_token	*elem;
+
+	if (alst == NULL || new == NULL)
+		return ;
+	if (*alst == NULL)
+		*alst = new;
+	else
+	{
+		elem = *alst;
+		while (elem->next != NULL)
+			elem = elem->next;
+		elem->next = new;
+	}
+}
+
+int	is_special_character(char c)
+{
+	if (c == '$' || c == '|' || c == '>' || c == '<' \
+		|| c == '?' || c == '(' || c == ')')
+		return (1);
+	return (0);
+}
+
+t_token	**split_into_tokens(char *input)
+{
+	t_token	*token;
+	t_token	**tokens;
+	char	**buff;
+	int		i;
+	int		j;
+
+	token = NULL;
+	tokens = NULL;
+	buff = ft_split(input, ' ');
+	i = -1;
+	j = -1;
+	// while (is_special_character(input[++j]))
+	// 	if (input[j] == ' ')
+	// 		printf("Space detected\n");
+	while (buff[++i])
+		{
+			printf("buff[%d]: %s\n", i, buff[i]);
+			if (i == 0)
+			{
+				token = ft_lstnew1(buff[i]);
+				tokens = &token;
+				printf("Token1 %d: %s\n", i, token->content);
+			}
+			else
+			{
+				token->next = ft_lstnew1(buff[i]);
+				ft_lstadd_back1(tokens, token);
+				printf("Token2 %d: %s\n", i, token->content);
+			}
+		}
+	return (tokens);
+}
+
 void	prompt_loop(void)
 {
 	char	*input;
+	t_token	**tokens = NULL;
 
 	while (1)
 	{
 		print_prompt();
 		input = read_command();
+		tokens = split_into_tokens(input);
+		printf("Token 0: %s\n", (*tokens)->content);
 		if (input == NULL)
 		{
 			break ;
