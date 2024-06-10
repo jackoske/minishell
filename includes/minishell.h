163850a6cd6aa135bc89de66a9a2eb2bc30d3056
VernihoraIvan <vernihorai@gmail.com>
2024-06-10 18:01:14 +0200

    refactoring main node structs, adding init_node function

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:11:41 by iverniho          #+#    #+#             */
-/*   Updated: 2024/06/10 12:55:43 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:31:39 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,21 @@
 
 # define PROMPT "minishell> "
 
-typedef enum e_types
+typedef struct s_node
 {
-	string = 1,
-	quote = 2,
-	red_in = 3,
-	red_out = 4,
-	append = 5,
-	heredoc = 6,
-	path = 7,
-	command = 8,
-	builtin = 9,
-	pipe_symb = 10,
-}					t_types;
-
-typedef struct s_token
-{
-	char			*content;
-	char			*cmd;
-	struct s_token	*next;
-	t_types			type;
-}					t_token;
-
-typedef struct s_full_command
-{
-	char			*full_command;
+	char			**full_command;
 	char			*command_path;
 	int				fd_in;
 	int				fd_out;
 	int				is_heredoc;
 	int				is_append;
-}					t_full_command;
+}					t_node;
 
 typedef struct s_mini
 {
 	char			**envp;
-	t_token			*token;
-	t_full_command	*full_command;
+	t_list			*node;
+	char			*current_dir;
 	int				exit_status;
 }					t_mini;
 
--- src/parser/prompt.c
+++ src/parser/prompt.c
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 21:09:11 by Jskehan           #+#    #+#             */
-/*   Updated: 2024/06/10 13:10:45 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:56:54 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,39 @@ static char	*read_command(void)
 	return (input);
 }
 
+static t_node	*init_node(void)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->full_command = NULL;
+	node->command_path = NULL;
+	node->fd_in = 0;
+	node->fd_out = 1;
+	node->is_heredoc = 0;
+	node->is_append = 0;
+	return (node);
+}
+
+void	create_nodes(char **input, t_mini *mini)
+{
+	t_list	*commands;
+	int		i;
+
+	i = -1;
+	(void)mini;
+	while (input && input[++i])
+	{
+		if (i == 0 || (input[i][0] == '|' && input[i + 1] && input[i + 1][0]))
+		{
+			ft_lstadd_back(&commands, ft_lstnew(init_node()));
+			printf("inside create_nodes: %s\n", input[i]);
+		}
+	}
+}
+
 void	prompt_loop(t_mini *mini)
 {
 	char	*input;
@@ -54,6 +87,7 @@ void	prompt_loop(t_mini *mini)
 			continue ;
 		}
 		tokenizedInput = ft_remove_quotes(tokenize_input(input, &mini));
+		create_nodes(tokenizedInput, mini);
 		i = -1;
 		while (tokenizedInput && tokenizedInput[++i])
 			printf("tokenizedInput very last step: %s\n", tokenizedInput[i]);
