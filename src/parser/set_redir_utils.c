a9bfd6d69fde33c33348a6b6e1fad73335571c5b
VernihoraIvan <vernihorai@gmail.com>
2024-06-13 12:50:55 +0200

    updating redirection handling functions

--- src/parser/set_redir_utils.c
+++ src/parser/set_redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:44:29 by iverniho          #+#    #+#             */
-/*   Updated: 2024/06/12 18:43:12 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/06/13 12:46:53 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ W_OK == -1
 
 static int	check_access(char *path, int mode)
 {
-	// printf("path: %s\n", path);
 	// printf("accces F inside check: %d\n", access(path, F_OK));
 	// printf("accces R inside check: %d\n", access(path, R_OK));
 	// printf("accces W inside check: %d\n", access(path, W_OK));
@@ -45,10 +44,9 @@ static int	check_access(char *path, int mode)
 	}
 	else if (mode == 2)
 	{
-		// printf("acces %d\n", access(path, W_OK));
 		if (access(path, W_OK) == -1)
 			return (printf("permission denied\n"), 0);
-		return (1);
+		return (printf("permission gained\n"),1);
 	}
 	return (1);
 }
@@ -62,19 +60,14 @@ static t_node	*get_redir_out(t_node *node, char *input, char **full_command, int
 		node->fd_out = -1;
 		return (node);
 	}
-	while(full_command[++(*(*i))])
-	{
-		if (full_command[(*(*i))][0] == '>')
-			break;
-	}
+	++(*(*i));
 	if (!check_access(full_command[(*(*i))], 2))
 	{
 		node->fd_out = -1;
 		return (node);
 	}
-	// printf("full_command[i]: %s\n", full_command[(*(*i))]);
 	// printf("node->fd_out: %d\n", node->fd_out);
-	node->fd_out = open(full_command[++(*(*i))], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	node->fd_out = open(full_command[(*(*i))], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	// printf("after\nnode->fd_out: %d\n", node->fd_out);
 	return (node);
 }
@@ -88,19 +81,14 @@ static t_node	*get_append_out(t_node *node, char *input, char **full_command, in
 		node->fd_out = -1;
 		return (node);
 	}
-	while (full_command[++(*(*i))])
-	{
-		if (full_command[(*(*i))][0] == '>' && full_command[(*(*i))][1] == '>')
-			break;
-	}
+	++(*(*i));
 	if (!check_access(full_command[(*(*i))], 2))
 	{
 		node->fd_out = -1;
 		return (node);
 	}
-	// printf("full_command[i]: %s\n", full_command[(*(*i))]);
 	// printf("node->fd_out: %d\n", node->fd_out);
-	node->fd_out = open(full_command[++(*(*i))], O_WRONLY | O_CREAT | O_APPEND, 0644);
+	node->fd_out = open(full_command[(*(*i))], O_WRONLY | O_CREAT | O_APPEND, 0644);
 	// printf("after\nnode->fd_out: %d\n", node->fd_out);
 	return (node);
 }
@@ -115,20 +103,26 @@ static t_node	*get_redir_heredoc(t_node *node, char *input, char **full_command,
 
 static t_node	*get_redir_in(t_node *node, char *input, char **full_command, int **i)
 {
-	(void)input;
-	if (!check_access(full_command[++(*(*i))], 1))
+	if (!input)
+	{
+		node->fd_out = -1;
+		return (node);
+	}
+	++(*(*i));
+	if (!check_access(full_command[(*(*i))], 1))
 	{
 		node->fd_in = -1;
 		return (node);
 	}
+	// printf("node->fd_in: %d\n", node->fd_in);
 	node->fd_in = open(full_command[(*(*i))], O_RDONLY);
+	// printf("after\nnode->fd_in: %d\n", node->fd_in);
 	return (node);
 }
 t_node	*set_redir(t_node *node, char *input, char **full_command, int *i)
 {
 	if (input[0])
 	{
-		// printf("input: %s\n", input);
 		if (input[0] == '>' && input[1] == '>')
 			node = get_append_out(node, input, full_command, &i);
 		else if (input[0] == '>')
@@ -140,8 +134,5 @@ t_node	*set_redir(t_node *node, char *input, char **full_command, int *i)
 		else if (input[0] != '|')
 			node->full_command = ft_add_row_2d_array(node->full_command, input);
 	}
-	// printf("inside set_redir full_command: %s\n", node->full_command[0]);
-	// 	printf("inside set_redir full_command: %s\n", node->full_command[1]);
-
 	return (node);
 }
