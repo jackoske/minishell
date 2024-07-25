d1fccebdcdf06c7ca23cac7fae804517a1487e67
VernihoraIvan <vernihorai@gmail.com>
2024-07-25 16:12:38 +0200

    initial commit

--- src/execution/builtin_utils.c
+++ src/execution/builtin_utils.c
@@ -6,7 +6,7 @@
 /*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 10:43:33 by Jskehan           #+#    #+#             */
-/*   Updated: 2024/07/24 16:29:11 by iverniho         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:09:07 by iverniho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,55 @@ void	show_last_command_status(t_mini *mini)
 	ft_putchar_fd('\n', STDOUT_FILENO);
 }
 
+void	mini_env(t_mini *mini)
+{
+	int	i;
+
+	i = 0;
+	while (mini->envp[i])
+	{
+		ft_putstr_fd(mini->envp[i], STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		i++;
+	}
+}
+
+void	mini_export(char **args, t_mini *mini)
+{
+	// int		i;
+	char	*key;
+	char	*value;
+	char	**temp;
+	int		len;
+	char	*new_env;
+
+	if (ft_2d_array_len(args) < 2)
+		return ;
+	temp = ft_split(*(args + 1), '=');
+	key = ft_strdup(temp[0]);
+	value = ft_strdup(temp[1]);
+	ft_free_2d_array(&temp);
+
+	// len = ft_strlen(args);
+	len = ft_2d_array_len(mini->envp);
+
+	// printf("args: %s\n", args);
+	printf("len: %d\n", len);
+	new_env = ft_strjoin(key, ft_strjoin("=", value));
+	printf("new_env: %s\n", new_env);
+	ft_add_row_2d_array(mini->envp, new_env);
+	ft_print_2d_array_fd(mini->envp, STDOUT_FILENO);
+	printf("last env: %s\n", mini->envp[len - 1]);
+
+}
+
 // Execute built-in commands
 void	execute_builtin(t_mini *mini, t_cmd *cmd)
 {
+	printf("cmd->full_command[0]: %s\n", cmd->full_command[0]);
+
+	// ft_print_2d_array_fd(cmd->full_command, STDOUT_FILENO);
+
 	if (ft_strcmp(cmd->full_command[0], "echo") == 0)
 		mini_echo(cmd);
 	else if (ft_strcmp(cmd->full_command[0], "pwd") == 0)
@@ -52,11 +98,11 @@ void	execute_builtin(t_mini *mini, t_cmd *cmd)
 		mini_exit(cmd->full_command, mini);
 	else if (ft_strcmp(cmd->full_command[0], "$?") == 0)
 		show_last_command_status(mini);
-	// else if (ft_strcmp(cmd->full_command[0], "export") == 0)
-	// 	mini_export(cmd->full_command, mini);
+	else if (ft_strcmp(cmd->full_command[0], "export") == 0)
+		mini_export(cmd->full_command, mini);
 	// else if (ft_strcmp(cmd->full_command[0], "unset") == 0)
 	// 	mini_unset(cmd->full_command, mini);
-	// else if (ft_strcmp(cmd->full_command[0], "env") == 0)
-	// 	mini_env(mini);
+	else if (ft_strcmp(cmd->full_command[0], "env") == 0)
+		mini_env(mini);
 	mini->exit_status = 0;
 }
